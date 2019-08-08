6b233a5643d1e76aa21a3a90debdb04989b90646
Einar Jonsson <einarjonsson@mac.com>
2019-08-08 09:51:01 +0200

    gifs are now larger

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -12,7 +12,7 @@ class Gif extends Component {
   }
 
   render() {
-    const src = `https://media.giphy.com/media/${this.props.id}/200w.gif`;
+    const src = `https://media.giphy.com/media/${this.props.id}/giphy.gif`;
     return (
       <img src={src} alt="" className="gif" onClick={this.handleClick} />
     );
